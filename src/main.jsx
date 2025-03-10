be427a63addfa6d04f9fd1a253e566daa595bc63
Zhiyar_Qaradaghi <zhiyar.burhan2003@gmail.com>
2025-03-10 23:38:44 +0300

    reset

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+import { DiscussionsProvider } from "./context/Discussions";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <DiscussionsProvider>
+      <App />
+    </DiscussionsProvider>
   </React.StrictMode>
 );
